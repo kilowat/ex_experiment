f5be05b1273504d24445c5a1d04697e3e52162b2
Kilowat <kadantsev.sergey@gmail.com>
2025-12-21 14:03:40 +0300

    fix

--- src/scenes/LevelScene.ts
+++ src/scenes/LevelScene.ts
@@ -9,47 +9,14 @@ export class LevelScene extends Scene {
     static route = 'level';
 
     onInitialize(engine: ex.Engine): void {
-        resources.ltdkMap.registerEntityIdentifierFactory('PlayerStart', (props) => {
-            const player = new Player({
-                name: 'player',
-                color: ex.Color.Red,
-                anchor: ex.vec(props.entity.__pivot[0], props.entity.__pivot[1]),
-                width: props.entity.width,
-                height: props.entity.height,
-                pos: props.worldPos,
-                z: props.layer.order
-            });
-            return player;
-        });
-        // Provide a type to the plugin to use for a specific entity identifier
-        // Player.ts
-        resources.ltdkMap.addToScene(this, {
-            pos: ex.vec(0, 0),
-            levelFilter: ['Level_0']
-        });
-        const player = this.world.entityManager.getByName('player')[0];
-        //this.add(player);
+        this._loadLtdkLevel();
+        this._createPlayer();
+        this._buildIntGrid();
+        this._createBackground();
+    }
 
-        if (player instanceof Player) {
-            // this.camera.clearAllStrategies();
-            this.camera.strategy.lockToActor(player);
-
-            // this.camera.strategy.radiusAroundActor(player, 100,);
-
-            const bounds = resources.ltdkMap.getLevelBounds(['Level_0']);
-            this.camera.strategy.limitCameraBounds(bounds);
-            this.add(player);
-        }
-
-        const intGrid = resources.ltdkMap.getIntGridLayers("level_0")[0];
-
-        intGridBuilder({
-            layer: intGrid,
-            onBuild: (actor) => this.add(actor)
-        })
-
+    private _createBackground() {
         const level = resources.ltdkMap.getLevel('Level_0')?.ldtkLevel!;
-
         //const bgNewaImage = level.fieldInstances.find(f => f.__identifier === 'BG_NEAR')?.__value;
         const bgNear = resources.exteriorParallaxBG1Src;
         const bgNearParalxVlue = level.fieldInstances.find(f => f.__identifier === 'BG_NEAR_PARALAX')?.__value ?? 0.2
@@ -72,8 +39,6 @@ export class LevelScene extends Scene {
         bgNearActor.addComponent(new ex.ParallaxComponent(ex.vec(0.2, 1)))
         this.add(bgNearActor);
 
-
-
         const bgFarActor = new ex.Actor({
             anchor: ex.vec(0, 0),
             pos: ex.vec(-100, 0),
@@ -91,7 +56,52 @@ export class LevelScene extends Scene {
         bgFarActor.addComponent(new ex.ParallaxComponent(ex.vec(0.1, 1)))
         this.add(bgFarActor);
 
+    }
+    private _buildIntGrid() {
+        const intGrid = resources.ltdkMap.getIntGridLayers("level_0")[0];
 
+        intGridBuilder({
+            layer: intGrid,
+            onBuild: (actor) => this.add(actor)
+        })
+
+    }
+    private _createPlayer() {
+        const player = this.world.entityManager.getByName('player')[0];
+        //this.add(player);
+
+        if (player instanceof Player) {
+            // this.camera.clearAllStrategies();
+            this.camera.strategy.lockToActor(player);
+
+            // this.camera.strategy.radiusAroundActor(player, 100,);
+
+            const bounds = resources.ltdkMap.getLevelBounds(['Level_0']);
+            this.camera.strategy.limitCameraBounds(bounds);
+            this.add(player);
+        }
+
+    }
+
+    private _loadLtdkLevel() {
+        resources.ltdkMap.registerEntityIdentifierFactory('PlayerStart', (props) => {
+            const player = new Player({
+                name: 'player',
+                color: ex.Color.Red,
+                anchor: ex.vec(props.entity.__pivot[0], props.entity.__pivot[1]),
+                width: props.entity.width,
+                height: props.entity.height,
+                pos: props.worldPos,
+                z: props.layer.order
+            });
+            return player;
+        });
+        // Provide a type to the plugin to use for a specific entity identifier
+        // Player.ts
+        resources.ltdkMap.addToScene(this, {
+            pos: ex.vec(0, 0),
+            levelFilter: ['Level_0']
+        });
     }
 
     onActivate(context: SceneActivationContext<unknown>): void {
@@ -101,5 +111,7 @@ export class LevelScene extends Scene {
     onDeactivate(context: SceneActivationContext): void {
 
     }
+
+
 }
 
